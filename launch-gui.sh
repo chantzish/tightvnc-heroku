78955cafe2ec51df4a6ab5c41d73a4ba93033753
chantzish <chantzish@gmail.com>
2020-01-04 00:39:06 +0200

    make more versatile

--- launch-gui.sh
+++ launch-gui.sh
@@ -15,7 +15,7 @@ printf "%s" "$VNC_PASS" | vncpasswd -f > /home/user/.vnc/passwd
 printf "%s\n" "$HEROKU_LOGIN" > .netrc
 printf "%s" "$IDENTITY" > .ssh/id_rsa
 printf "%s" "$GDRIVE_TOKEN" > .gdrive/token_v2.json
-gdrive download $TELEGRAM_LOCAL
+gdrive download `gdrive list -m 1000 | grep telegram.local.tar.gz | awk '{print $1}'`
 tar zxf telegram.local.tar.gz
 git config --global user.email "chantzish@gmail.com"
 git config --global user.name "chantzish"
@@ -23,9 +23,9 @@ git config --global credential.helper store
 printf "%s\n" "$GIT_CREDENTIALS" > .git-credentials
 
 vncserver -geometry 1536x754 :1
-gdrive download `gdrive list -m 200 | grep vmlinuz | awk '{print $1}'`
-gdrive download `gdrive list -m 200 | grep initrd | awk '{print $1}'`
-gdrive download `gdrive list -m 200 | grep arm.qcow2 | awk '{print $1}'`
-qemu-system-arm -M virt -m 512M -kernel vmlinuz-4.9.0-11-armmp-lpae -initrd initrd.img-4.9.0-11-armmp-lpae -append root=/dev/vda2 -drive if=none,file=arm.qcow2,format=qcow2,id=hd -device virtio-blk-device,drive=hd -netdev user,hostfwd=tcp:0.0.0.0:2201-:22,id=mynet -device virtio-net-device,netdev=mynet -serial mon:telnet:0.0.0.0:2300,server,nowait -vga none -display none -daemonize
+gdrive download `gdrive list -m 1000 | grep vmlinuz | awk '{print $1}'`
+gdrive download `gdrive list -m 1000 | grep initrd | awk '{print $1}'`
+gdrive download `gdrive list -m 1000 | grep arm.qcow2 | awk '{print $1}'`
+qemu-system-arm -M virt -m 512M -kernel vmlinuz* -initrd initrd* -append root=/dev/vda2 -drive if=none,file=arm.qcow2,format=qcow2,id=hd -device virtio-blk-device,drive=hd -netdev user,hostfwd=tcp:0.0.0.0:2201-:22,id=mynet -device virtio-net-device,netdev=mynet -serial mon:telnet:0.0.0.0:2300,server,nowait -vga none -display none -daemonize
 /opt/noVNC/utils/websockify/run 8081 localhost:2201 --heartbeat=45 &
 /opt/noVNC/utils/websockify/run 8082 localhost:2300 --heartbeat=45 &
