485e68b740ecca7645ac5d1caa84acfd22d9c9b0
chantzish <chantzish@gmail.com>
2020-03-11 12:27:31 +0200

    fixed gdrive & git

--- launch-gui.sh
+++ launch-gui.sh
@@ -3,6 +3,7 @@
 export HOME=/home/user
 export USER=`whoami`
 export LANG=en_IL
+export PATH=/usr/games:$PATH
 #export JAVA_HOME=/usr/lib/jvm/default-java
 
 # fix according to site
@@ -14,10 +15,21 @@ chmod +x /usr/local/sbin/stop.sh
 printf "%s" "$VNC_PASS" | vncpasswd -f > /home/user/.vnc/passwd
 printf "%s" "$HEROKU_LOGIN" > .netrc
 printf "%s" "$IDENTITY" > .ssh/id_rsa
+cd gdrive
+sed -i 's/const ClientId = ".*.apps.googleusercontent.com"/const ClientId = "'"$GDRIVE_CLIENT_ID"'"/' handlers_drive.go
+sed -i 's/const ClientSecret = ".*"/const ClientSecret = "'"$GDRIVE_CLIENT_SECRET"'"/' handlers_drive.go
+go get github.com/prasmussen/gdrive
+go build
+cp gdrive /usr/local/sbin/gdrive
+cd ..
 printf "%s" "$GDRIVE_TOKEN" > .gdrive/token_v2.json
 gdrive download $TELEGRAM_LOCAL
 tar zxf telegram.local.tar.gz
 printf "%s" "$GIT_CONF" > .gitconfig
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+git config --global credential.helper store
+git config --global color.ui auto
 printf "%s" "$GIT_CREDENTIALS" > .git-credentials
 
 vncserver -geometry 1536x754 :1
