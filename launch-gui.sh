ff8d2731ca083c847f7f4bbc82759f26768b6582
chantzish <chantzish@gmail.com>
2020-06-16 18:31:36 +0300

    fixed script

--- launch-gui.sh
+++ launch-gui.sh
@@ -7,12 +7,13 @@ export PATH=/usr/games:$PATH
 #export JAVA_HOME=/usr/lib/jvm/default-java
 
 # fix according to site
-echo '#!/bin/sh\n\nwhile :; do wget '$APP_NAME'.herokuapp.com -q -O /dev/null -o /dev/null; sleep 4m; done &' | tee /usr/local/sbin/stop.sh
+echo -e '#!/bin/sh\n\nwhile :; do wget '$APP_NAME'.herokuapp.com -q -O /dev/null -o /dev/null; sleep 4m; done &' | tee /usr/local/sbin/stop.sh
 chmod +x /usr/local/sbin/stop.sh
 
 #mkdir -m 1777 /tmp/.X11-unix
 #mkdir .vnc
 printf "%s" "$VNC_PASS" | vncpasswd -f > /home/user/.vnc/passwd
+chmod 600 .vnc/passwd
 printf "%s" "$HEROKU_LOGIN" > .netrc
 printf "%s" "$IDENTITY" > .ssh/id_rsa
 
