af135281877782b7cac259e9efe969f1586fa10c
chantzish <chantzish@gmail.com>
2020-06-18 17:42:20 +0300

    fix env vars and move code to launch-gui.sh

--- launch.sh
+++ launch.sh
@@ -4,12 +4,7 @@
 export DOLLAR='$'
 export HOME=/home/user
 export USER=`whoami`
-export LANG=en_IL
+export LANG=en_US.UTF-8
+export PATH="$PATH:/home/user/.local/bin:/usr/games:/usr/local/games"
 envsubst < nginx.template > /etc/nginx/sites-enabled/default
-touch /opt/noVNC/`whoami`
-cd /opt/noVNC && ./utils/launch.sh --listen 6080 --vnc localhost:5901 &
-/opt/noVNC/utils/websockify/run 6090 localhost:2200 --heartbeat=45 &
-printf "%s" "$AUTH_KEYS" > .ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-/usr/sbin/sshd -p 2200 -o AuthorizedKeysFile=/home/user/.ssh/authorized_keys &
 nginx -g 'daemon off;'
