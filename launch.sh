e8cd117f7e947ae0031d86dafb06d1418c0e65b4
chantzish <chantzish@gmail.com>
2020-07-01 20:00:05 +0300

    add SHELL env var

--- launch.sh
+++ launch.sh
@@ -6,5 +6,6 @@ export HOME=/home/user
 export USER=`whoami`
 export LANG=en_US.UTF-8
 export PATH="$PATH:/home/user/.local/bin:/usr/games:/usr/local/games"
+export SHELL=/usr/bin/bash
 envsubst < nginx.template > /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
