24e6735973a41add31c36a3acf6361b5459e1f02
chantzish <chantzish@gmail.com>
2020-07-02 20:15:13 +0300

    change root user to normal auth instead of socket
    socket is authenticated by matching operating system user name

--- mariadb.sh
+++ mariadb.sh
@@ -2,4 +2,9 @@
 
 sed -i "s/user='mysql'/user='$USER'/" /usr/bin/mysqld_safe
 sed -i "s/user                    = mysql/user                    = $USER/" /etc/mysql/mariadb.conf.d/50-server.cnf
+#rm -r /var/lib/mysql/*
+#touch /var/lib/mysql/debian-10.3.flag
+find /var/lib/mysql -mindepth 1 -not -name *flag -delete
+bash /usr/bin/mysql_install_db --auth-root-authentication-method=normal --rpm --cross-bootstrap --user=$USER --disable-log-bin
+# bash /usr/bin/mysql_install_db --auth-root-authentication-method=normal --rpm --cross-bootstrap --user=$USER --disable-log-bin  --skip-test-db
 service mysql start
