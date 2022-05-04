8d5e02170f44589168c0017422b154f1a7dc05dc
chantzish <chantzish@gmail.com>
2022-05-04 07:45:49 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-export DOLLAR='$'
-export HOME=/home/user
-export USER=`whoami`
-export LANG=en_US.UTF-8
-export PATH="$PATH:/home/user/.local/bin:/usr/games:/usr/local/games"
-export SHELL=/usr/bin/bash
 envsubst < nginx.template > /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
