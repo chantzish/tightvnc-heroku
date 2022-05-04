063d6e7707b04f8dd86a13922bb77dd8409a2b03
chantzish <chantzish@gmail.com>
2022-05-04 08:06:39 +0300

    Update launch-gui.sh

--- launch-gui.sh
+++ launch-gui.sh
@@ -2,24 +2,10 @@
 
 export HOME=/home/user
 export USER=`whoami`
-export LANG=en_IL
-#export JAVA_HOME=/usr/lib/jvm/default-java
+export LANG=en_US.UTF-8
+export PATH="$PATH:/home/user/.local/bin:/usr/games:/usr/local/games"
+export SHELL=/usr/bin/bash
 
-# fix according to site
-echo '#!/bin/sh\n\nwhile :; do wget '$APP_NAME'.herokuapp.com -q -O /dev/null -o /dev/null; sleep 4m; done &' | tee /usr/local/sbin/stop.sh
-chmod +x /usr/local/sbin/stop.sh
-
-#mkdir -m 1777 /tmp/.X11-unix
-#mkdir .vnc
-printf "%s" "$VNC_PASS" | vncpasswd -f > /home/user/.vnc/passwd
-printf "%s\n" "$HEROKU_LOGIN" > .netrc
-printf "%s" "$IDENTITY" > .ssh/id_rsa
-printf "%s" "$GDRIVE_TOKEN" > .gdrive/token_v2.json
-gdrive download $TELEGRAM_LOCAL
-tar zxf telegram.local.tar.gz
-git config --global user.email "chantzish@gmail.com"
-git config --global user.name "chantzish"
-git config --global credential.helper store
-printf "%s\n" "$GIT_CREDENTIALS" > .git-credentials
-
-vncserver -geometry 1536x754 :1
+printf "%s\n" "$LAUNCH_RUNTIME" > launch-runtime.sh
+chmod +x launch-runtime.sh
+./launch-runtime.sh
