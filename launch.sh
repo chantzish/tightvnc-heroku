e960f060bf989cf8b53d12e81bed330a6d632ce5
chantzish <chantzish@gmail.com>
2022-09-06 13:05:08 +0300

    fix2 launch for fly.io

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export DOLLAR='$'
-envsubst < nginx.template > /etc/nginx/sites-enabled/default
+echo 1234 | sudo -S /bin/sh -c 'envsubst < nginx.template > /etc/nginx/sites-enabled/default'
 echo 1234 | sudo -S nginx -g 'daemon off;'
