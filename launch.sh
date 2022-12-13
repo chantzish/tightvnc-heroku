de8514560e08afe8d797fa3324adee162078f11f
chantzish <chantzish@gmail.com>
2022-12-13 07:24:31 +0200

    run as root part 2

--- launch.sh
+++ launch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-export DOLLAR='$'
-echo 1234 | sudo -E -S /bin/sh -c 'envsubst < nginx.template > /etc/nginx/sites-enabled/default'
-echo 1234 | sudo -S nginx -g 'daemon off;'
+#export DOLLAR='$'
+#echo 1234 | sudo -E -S /bin/sh -c 'envsubst < nginx.template > /etc/nginx/sites-enabled/default'
+#echo 1234 | sudo -S nginx -g 'daemon off;'
+
+printf "%s\n" "$LAUNCH_START" > launch-start.sh
+chmod +x launch-start.sh
+./launch-start.sh
